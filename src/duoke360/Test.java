ef43a95f596a0f6635da7d62ccaf680bef61cf99
Manurios-L <liuyang20001018@qq.com>
2024-05-19 11:39:08 +0800

    bug-fix v3

--- src/duoke360/Test.java
+++ src/duoke360/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("hello java fix");
         System.out.println("hello 555555566666");
         System.out.println("666666666666");
+        System.out.println("877777777");
     }
 }
