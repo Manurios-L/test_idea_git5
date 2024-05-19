866f13d4d45bb15e3317013ef5d1913ab47706d3
Manurios-L <liuyang20001018@qq.com>
2024-05-19 10:55:20 +0800

    v2

--- src/duoke360/Test.java
+++ src/duoke360/Test.java
@@ -3,5 +3,6 @@ package duoke360;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("hello java v1");
+        System.out.printf("hello java v2");
     }
 }
